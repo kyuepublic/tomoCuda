a992f75bb8bdd0b0a3a3727d2333f3b22100408b
Ke Yue <kyue@aps.anl.gov>
2016-12-08 18:07:00 -0600

    update test script

--- test/test.py
+++ test/test.py
@@ -5,11 +5,11 @@ import timeit
 import tomocuda
 import tomopy
 
-def testMedianFilter2D():
+def testMedianFilter2D(size):
     '''test with random array, loop outside with a 2d cuda kernel'''
     # prjsize is z, imsize is x, y.
 
-    size = 14 # window size for the filter
+    # size = 13 # window size for the filter
     imsizex =2048 # image size for the input
     imsizey = 2048
     prjsize= 1
@@ -19,20 +19,20 @@ def testMedianFilter2D():
     resultscpu = np.zeros(shape=(prjsize,imsizey,imsizex), dtype=np.float32)
 
     # create combined noise matrix 3D
-    for step in range (prjsize):
-        # im_noise = np.arange( 10, imsizey*imsizex*step+10, step ).reshape(imsizey, imsizex)
-        im_noise = np.random.rand(imsizey, imsizex)
+    for step in range (5, 5 + prjsize):
+        im_noise = np.arange( 10, imsizey*imsizex*step+10, step ).reshape(imsizey, imsizex)
+        # im_noise = np.random.rand(imsizey, imsizex)
         im_noise = im_noise.astype(np.float32)
-        combinedMed[step]=im_noise
+        combinedMed[step-5]=im_noise
 
     start = timeit.default_timer()
-    resultscpu= tomopy.median_filter(combinedMed,size=size)
+    resultscpu= tomopy.median_filter_cuda(combinedMed,size=size)
     stop = timeit.default_timer()
     diff2 = stop - start
     print("end cuda filter", diff2)
 
     start = timeit.default_timer()
-    resultscuda = tomopy.median_filter(combinedMed,size=size)
+    resultscuda = tomopy.median_filter(combinedMed,size=size, ncore=1)
     stop = timeit.default_timer()
     diff1 = stop - start
 
@@ -42,11 +42,11 @@ def testMedianFilter2D():
 
     print not np.any(resultscuda-resultscpu)
 
-def testOutlierRemoval():
+def testOutlierRemoval(size):
     '''test with random array, loop outside with a 2d cuda kernel'''
     # prjsize is z, imsize is x, y.
 
-    size = 9 # window size for the filter
+    # size = 13 # window size for the filter
     imsizex =2048 # image size for the input
     imsizey = 2048
     prjsize= 1
@@ -82,7 +82,9 @@ def testOutlierRemoval():
 
 if __name__ == '__main__':
 
-    # testMedianFilter2D()
+    size = 2
 
-    testOutlierRemoval()
+    testMedianFilter2D(size)
+
+    testOutlierRemoval(size)
 
